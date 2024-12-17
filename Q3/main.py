c9e1ba5331022d8d6b749533cf2824ffb0ed0199
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2024-12-17 23:02:15 +0000

    1st attempt Q3, comments added to highlight approach

---
+++ Q3/main.py
@@ -0,0 +1,10 @@
+import re
+
+"""Read file, extract the two numbers from the mul(x,y) section whenever a match is found via regex, move to the next section"""
+
+
+def multiply(x, y):
+    return x * y
+
+
+# def read_input(input_file):
