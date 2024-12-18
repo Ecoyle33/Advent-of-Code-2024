48047d929e959e2b607d3ce2be4d51c2875e8503
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2024-12-18 00:10:13 +0000

    Part 1 complete, solution copied again

--- Q3/main.py
+++ Q3/main.py
@@ -7,4 +7,25 @@ def multiply(x, y):
     return x * y
 
 
-# def read_input(input_file):
+def read_input(input_file):
+    with open(input_file, "r") as file:
+        lines = file.readlines()
+
+    return lines
+
+
+def sum_multiplication(memory):
+    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    total = 0
+
+    for element in memory:
+        multiplication = re.findall(pattern, element)
+
+        for x, y in multiplication:
+            total += int(x) * int(y)
+    return total
+
+
+input_text = read_input("Q3\input.txt")
+
+print(sum_multiplication(input_text))
